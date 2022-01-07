2ffe42dc21d6a05233cd42441b10bd842b4505e9
Henry <hspradley91@gmail.com>
2022-01-07 16:59:19 -0600

    Finished exercise 9-10 and created a module 'Restaurant'

---
+++ Chpt 9/9-10_importedrestaurant.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  7 16:56:29 2022
+
+@author: Hank
+"""
+
+from Restaurant import Restaurant as Res
+
+terranova = Res('Terranova', 'Italian Fusion')
+
+terranova.describe_restaurant()
+terranova.open_restaurant()
