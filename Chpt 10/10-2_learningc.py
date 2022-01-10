00b204e3ab8d8cf0b3fd2d2b61bc86a38e77d587
Henry <hspradley91@gmail.com>
2022-01-09 22:18:16 -0600

    Finished Exercise 10-2

---
+++ Chpt 10/10-2_learningc.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  9 22:16:27 2022
+
+@author: Hank
+"""
+
+file_name = "C:/Users/Hank/python_learning/PyCC/Chpt 10/what_i've_learned.txt"
+
+# Replacing every instance of 'Python' with 'C'
+print("\n\n ~ Creating a list and printing outside with block: \n")
+with open(file_name) as file:
+    lines = file.readlines()
+    
+for line in lines:
+    print(line.replace('python','C'))
