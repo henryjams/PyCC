4772c76769c8fed0f288fcf649cc79a71b1572f7
Henry <hspradley91@gmail.com>
2022-01-14 19:10:35 -0600

    Finished exercise 10-9

---
+++ Chpt 10/10-9_silentcatsanddogs.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 14 18:55:37 2022
+
+@author: Hank
+"""
+
+file_list = ['cats.txt', 'dogs.txt']
+
+for file in file_list:
+    try:
+        with open(file) as f:
+            contents = f.read()
+            print(contents + "\n")
+    except FileNotFoundError:
+        pass
